31820ff5e37e0f3e1ae72ae6d5c1032fe297213a
chriscutting <34825551+chriscutting@users.noreply.github.com>
2018-02-20 21:28:40 -0500

    basic functions
    learning basic arguments/functions

---
+++ Scripts/functions.js
@@ -0,0 +1,13 @@
+function makeWords() {
+  console.log("i");
+  console.log("like");
+  console.log("functions")
+}
+
+function doMath(num) {
+  console.log(num * num);
+}
+
+function sayHello(name) {
+  console.log('Hi there ' + name + "!");
+}
