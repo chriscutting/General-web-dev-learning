5ff576cd33678f76736e5c3aec05481948114acd
chriscutting <34825551+chriscutting@users.noreply.github.com>
2018-02-20 20:30:08 -0500

    forloops
    practicing the forloops

--- Scripts/yet.js
+++ Scripts/yet.js
@@ -1,6 +1,6 @@
 var answer = prompt('Are we there yet?');
 
-while (answer !== "yes" + answer !== "yeah") {
+while (answer !== "yes" || answer !== "yeah") {
   var answer = prompt("Are we there yet?");
 }
 
