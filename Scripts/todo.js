86f1230a5464ca81254cfea8b7cb5564d7e9e18d
chriscutting <34825551+chriscutting@users.noreply.github.com>
2018-02-24 16:12:27 -0500

    todo list
    improving the todo list

--- Scripts/todo.js
+++ Scripts/todo.js
@@ -1,26 +1,55 @@
-var todos = ["Buy groceries , Walk the dog , Vaccuum floors"];
+var todos = ["Buy groceries", "Walk the dog" , "Vaccuum floors"];
 
 var input = prompt("What would you like to do?");
 
 
 while(input !== "quit"){
 //input
-if (input === "list"){
-  console.log("todos");
+if(input === "list")
+listToDos()
+
+
+else if(input === "new") {
+addToDo()
 }
+else if(input === "delete") {
+  deleteToDo()
+  //ask for index
+  //delete todos
 
-else if(input === "new"){
-  //add to todos
-  var newTodo = prompt("Enter new TODO");
-  todos.push(newTodo);
-}
-
-
-input = prompt("What would you like to do?");
 
 
   //ask again
 //Wrap it up.
-
+input = prompt("What do you need");
 }
 console.log("Goodbye!");
+
+//Listing function
+function listToDos(){
+  {
+    console.log("*********************")
+  }{  todos.forEach(function(todo, i){
+
+      console.log(i + ": " + todo);
+  });
+    console.log("*********************")
+  }
+}
+
+
+//Adding function
+
+
+function addToDo(){
+  var newTodo = prompt("Enter new TODO");
+  todos.push(newTodo);
+}
+
+//delete function
+function deleteToDo(){
+  var index = prompt("Enter index to delete");
+  //splice
+  todos.splice(index,1);
+  console.log("Deleted Todo");
+}
