3ca775cba3dd74eb6c3794b985a3114556541458
chriscutting <34825551+chriscutting@users.noreply.github.com>
2018-02-24 17:13:11 -0500

    arrays
    updated  arrays

---
+++ Scripts/arrayHw.js
@@ -0,0 +1,43 @@
+//print backwards
+/*
+function printReverse(arr){
+  for(var i = arr.length - 1; i >= 0, i--) {
+    console.log(arr[i]);
+  }
+}
+
+
+printreverse([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+*/
+//isUniform
+/*
+function isUniform(arr){
+  var first = arr[0];
+  for(var i = 1, i < arr.length, i++){
+    if(arr[i] !== first){
+      return false;
+    }
+  }
+  return true;
+} */
+//sum array
+/*
+function sumArray(arr){
+  total = 0;
+  arr.forEach(function(element){
+    total += element;
+  });
+  return total;
+}
+*/
+
+//max function()
+function max(arr){
+  var max = arr[0];
+  for(var = i, i < arr.length, i++);
+  if(arr[i]) > max{
+    max = arr[1];
+
+  }
+  return max;
+}
