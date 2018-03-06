4692208a498e023b9475081cae1cd03c4a416cf9
chriscutting <34825551+chriscutting@users.noreply.github.com>
2018-03-05 20:59:32 -0500

    color picking game
    adding random colors!

--- color picking game/colorpicking.js
+++ color picking game/colorpicking.js
@@ -1,20 +1,52 @@
-var colors= ['RGB(0, 65, 145)', "RGB(45, 65, 87)", "RGB(123, 23, 43)", 'RGB(67, 98, 23)', 'RGB(45, 155, 200)', 'RGB(98, 100, 125)']
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById('pickedColor')
+var messageDisplay = document.querySelector('#message')
+var h1 = document.querySelector('h1');
 colorDisplay.textContent =' ' + pickedColor;
 for(var i = 0; i< squares.length; i++){
 
   //add initial colors
   squares[i].style.backgroundColor = colors[i];
   squares[i].addEventListener("click", function(){
-    var clickedColor = this.style.backgroundColor;
-    if (clickedColor === pickedColor){
-      alert("You win!");
-    } else{
-        alert("No!");
+    var clickedColor = this.style.background;
+    if(clickedColor === pickedColor){
+      messageDisplay.textContent = "Correct!";
+      changeColors(clickedColor);
+      h1.style.backgroundColor = clickedColor;
+    } else {
+        this.style.backgroundColor = "#232323";
+        messageDisplay.textContent = "Try again";
+        console.log(pickedColor, clickedColor);
       }
-    })
-  };
+    });
+  }
 
-  //add clicklisteners
+function changeColors(color){
+  for(var i = 0; i < squares.length; i++)
+  squares[i].style.backgroundColor = color;
+}
+
+function pickColor(){
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+
+function generateRandomColors(num){
+  var arr = [];
+
+  for(var i = 0; i < num; i++)
+  arr.push(randomColor())
+  return arr;
+
+}
+
+function randomColor(){
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  return "RGB(" + r + ", " + g + ", " + b + ")" ;
+
+}
