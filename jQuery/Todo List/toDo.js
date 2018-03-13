513d3ad2d68b258cd16a5a6e700d79dd66b6be4f
chriscutting <34825551+chriscutting@users.noreply.github.com>
2018-03-12 20:55:18 -0400

    ToDo
    adding function/styles to the list app

--- jQuery/Todo List/toDo.js
+++ jQuery/Todo List/toDo.js
@@ -1,4 +1,28 @@
 //check off specific Todos via clicking
-$('li').click(function(){
-$(this).toggleClass("selected");
+$("ul").on("click", "li", function(){
+  $(this).toggleClass("selected");
+});
+
+$("ul").on('click', 'span', (function(event){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+}));
+
+
+$('input').keypress(function(event){
+  if(event.which === 13){
+    // Grab the text!
+    var todoText = $(this).val();
+    $(this).val("");
+    //make new li and add to ul
+    $("ul").append('<li><span class = "trash"><i class="far fa-trash-alt"></i></span> ' + todoText + '</li>')
+  }
+});
+
+
+
+$(".fa-plus-square").click(function(){
+  $("input").fadeToggle();
 });
